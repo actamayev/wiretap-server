af2c320f62080b1e296cce9f2073f3bae67e9062
Ariel Tamayev <actamayev@gmail.com>
2025-12-10 23:16:07 -0500

    Make events open

--- sql/selects.sql
+++ sql/selects.sql
@@ -7,7 +7,7 @@
 -- SELECT * FROM polymarket_outcome order by market_id;
 -- SELECT * FROM purchase_order;
 -- SELECT * FROM sale_order;
--- SELECT * FROM FROM "position";
--- SELECT * FROM portfolio_snapshot;
+-- SELECT * FROM "position" where wiretap_fund_uuid = 'eaf4100d-8fb2-48e4-b967-45ec1b5be050';
+-- SELECT * FROM portfolio_snapshot where wiretap_fund_uuid = 'eaf4100d-8fb2-48e4-b967-45ec1b5be050';
 -- SELECT * FROM polymarket_price_history;
 -- SELECT * FROM _prisma_migrations;
