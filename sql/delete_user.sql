8a384c769b757323db5369ca4a71f545eb61ef2b
lever-labs <thewiretapai@gmail.com>
2025-12-15 13:18:43 -0500

    Update delete_user.sql

--- sql/delete_user.sql
+++ sql/delete_user.sql
@@ -1,7 +1,7 @@
-DELETE FROM "position";
-DELETE FROM purchase_order;
-DELETE FROM sale_order;
-DELETE FROM portfolio_snapshot;
-DELETE from wiretap_fund;
-DELETE from login_history;
-DELETE FROM credentials;
+-- DELETE FROM "position";
+-- DELETE FROM purchase_order;
+-- DELETE FROM sale_order;
+-- DELETE FROM portfolio_snapshot;
+-- DELETE from wiretap_fund;
+-- DELETE from login_history;
+-- DELETE FROM credentials;
