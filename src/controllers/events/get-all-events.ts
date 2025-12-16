4cfd0e5fc2ebd4e66f3dff603f0ad428200910d3
lever-labs <thewiretapai@gmail.com>
2025-12-16 15:40:21 -0500

    Pagination

--- src/controllers/events/get-all-events.ts
+++ src/controllers/events/get-all-events.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express"
 import EventsCache from "../../classes/events-cache"
 
-export default async function getAllEvents(_req: Request, res: Response): Promise<Response> {
-	try {
-		const events = await EventsCache.getInstance().getEventsMetadataOrFetch()
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const EVENTS_PER_PAGE = 20
 
-		return res.status(200).json({ events } satisfies AllEventsResponse)
+export default async function getAllEvents(req: Request, res: Response): Promise<Response> {
+	try {
+		const offset = req.query.offset ? parseInt(req.query.offset as string, 10) : 0
+
+		const allEvents = await EventsCache.getInstance().getEventsMetadataOrFetch()
+
+		const paginatedEvents = allEvents.slice(offset, offset + EVENTS_PER_PAGE)
+
+		return res.status(200).json({ events: paginatedEvents } satisfies AllEventsResponse)
 	} catch (error) {
 		console.error(error)
 		return res.status(500).json({ error: "Internal Server Error: Unable to retrieve events" })
--- src/routes/events-routes.ts
+++ src/routes/events-routes.ts
@@ -3,11 +3,12 @@ import express from "express"
 import getAllEvents from "../controllers/events/get-all-events"
 import getSingleEvent from "../controllers/events/get-single-event"
 import validateEventIdInParams from "../middleware/request-validation/events/validate-event-id-in-params"
+import validateOffsetQueryParam from "../middleware/request-validation/events/validate-offset-query-param"
 import attachEventIdFromEventSlug from "../middleware/attach/attach-event-id-from-event-slug"
 
 const eventsRoutes = express.Router()
 
-eventsRoutes.get("/all-events", getAllEvents)
+eventsRoutes.get("/all-events", validateOffsetQueryParam, getAllEvents)
 eventsRoutes.get(
 	"/single-event/:eventSlug",
 	validateEventIdInParams,
